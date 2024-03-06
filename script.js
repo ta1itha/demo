7ae78eaa9feeca3ffbbc8d65435fbc174001a394
ta1itha <talithadev93@gmail.com>
2024-03-06 12:49:15 +0200

    add js demo

--- script.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-const dialogOpener = document.querySelector("h2");
-const dialog = document.querySelector("dialog");
-const dialogCloser = document.querySelector("button");
-
-dialogCloser.addEventListener("click", function () {
-  dialog.close();
-});
