788f3d419cf111dee378f9c8f504ed558acf3049
ta1itha <talithadev93@gmail.com>
2024-03-06 13:50:46 +0200

    add js

---
+++ script.js
@@ -0,0 +1,5 @@
+var productOnSpecial = "shoes";
+var price = 200;
+console.log("The product on special is " + productOnSpecial + " for $" + price);
+productOnSpecial = "handbags";
+console.log("The product on special is " + productOnSpecial + " for $" + price);
