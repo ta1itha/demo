87618823fe1b7285a6a4e072a515b5b07b46fce8
ta1itha <talithadev93@gmail.com>
2024-03-11 13:55:31 +0200

    add if else

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var price = 200;
 console.log("The product on special is " + productOnSpecial + " for $" + price);
 productOnSpecial = "handbags";
 console.log("The product on special is " + productOnSpecial + " for $" + price);
+
+// if statement
+
+//is a user logged in?
+var isTanerTired = false;
+var isTanerANirvanaFan = true;
+
+if (isTanerTired && isTanerANirvanaFan) {
+  console.log("Go take a nap and dream of Nirvana writing Javascript");
+} else {
+  console.log("Go practice if else statements while listening to Coldplay");
+}
